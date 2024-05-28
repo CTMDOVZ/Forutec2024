2c001dbfeeb1440138f04b90e5cbef62531ac19c
Mateo Espinoza <mateo.espinoza@utec.edu.pe>
2024-05-27 22:24:20 -0500

    TEST

---
+++ forutec/src/main/java/com/example/forutec/ForutecApplication.java
@@ -0,0 +1,13 @@
+package com.example.forutec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForutecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForutecApplication.class, args);
+	}
+
+}
---
+++ forutec/src/test/java/com/example/forutec/ForutecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forutec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForutecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
