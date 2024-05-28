f9a0983fc8c9bb2124e752f54dd2e2e73f9531b3
Mateo Espinoza <mateo.espinoza@utec.edu.pe>
2024-05-27 19:48:22 -0500

    upload deploy 3

--- src/test/java/com/example/forutec_pt1/Categoria/domain/CategoriaTest.java
+++
@@ -1,4 +0,0 @@
-package com.example.forutec_pt1.Categoria.domain;
-
-public class CategoriaTest {
-}
--- src/test/java/com/example/forutec_pt1/Categoria/infrastructure/CategoriaRepositoryTest.java
+++
@@ -1,4 +0,0 @@
-package com.example.forutec_pt1.Categoria.infrastructure;
-
-public class CategoriaRepositoryTest {
-}
